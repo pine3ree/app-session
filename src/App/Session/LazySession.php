a942ae44f74047e3ba4f46caa3b4173b724f5700
pine3ree <pine3ree@gmail.com>
2019-03-11 20:59:52 +0100

    php-doc

--- src/App/Session/LazySession.php
+++ src/App/Session/LazySession.php
@@ -44,7 +44,7 @@ final class LazySession extends AbstractSession implements SessionInterface
     /**
      * @param string $id The session identifier
      * @param bool $isNew Is this session a new session?
-     * @param Closure $dataFactory A callable facory for session container instantiation
+     * @param Closure $dataFactory A callable factory for lazy instantiation of the session container
      */
     public function __construct(string $id, bool $isNew, Closure $dataFactory)
     {
